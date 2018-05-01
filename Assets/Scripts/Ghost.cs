9fc3347f2a2cdbb8edd0bed5d0a858220ed21ada
Dan McElroy <daniel@mcelroys.net>
2018-05-01 14:30:33 +0200

    Added initial files.

---
+++ Assets/Scripts/Ghost.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class Ghost : MonoBehaviour
+{
+    [SerializeField]
+    private static Transform bench;
+
+    [SerializeField]
+    private readonly Sprite Sprite;
+    
+    [SerializeField]
+    private GhostBindable _boundTo;
+    
+    public GhostBindable BoundTo
+    {
+        get
+        {
+            return _boundTo;
+        }
+        set
+        {
+            transform.SetParent(value?.transform ?? bench);
+            _boundTo = value;
+        }
+    }
+
+    [SerializeField]
+    private IEnumerable<GhostPower> _powers;
+
+	// Use this for initialization
+	void Start ()
+    {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		
+	}
+}
---
+++ Assets/Scripts/GhostPower.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GhostPower : MonoBehaviour
+{
+    public int Cost { get; set; }
+
+    public string Name { get; set; }
+
+	// Use this for initialization
+	void Start ()
+    {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		
+	}
+}
